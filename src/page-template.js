5f97b26cd51601f9b22eded6dc99eea941f13443
White Tiger <dayley.zimmermann@gmail.com>
2022-02-12 20:42:04 -0500

    final additions, page-template finished and css added

--- src/page-template.js
+++ src/page-template.js
@@ -1,101 +1,90 @@
-//Generate the html here
+const html = [];
 
-// const generateManager = managerInfo => {
-//   return `
-//       ${managerInfo
-//         .map(({ managerName, managerId, managerEmail, managerOfficeNumber }) => {
-//           return `
-//           <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-//             <h3 class="portfolio-item-title text-light">${managerName}</h3>
-//             <h5 class="portfolio-languages">
-//               ID: ${managerId}
-//             </h5>
-//             <p>${managerEmail}</p>
-//             <p>${managerOfficeNumber}</p>
-//           </div>
-//         `;
-//         })
-//         .join('')}
-//       </div>
-//     </section>
-//   `;
-// };
+function generateTeamMembers(data) {
+  function generateManager(manager) {
+    return `
+    <div class="employeeBorder">
+      <div class="employeeHeader">
+        <label class="eName">${manager.name}</label>
+        <label class="jobRole">${manager.getRole()}</label>
+      </div>
+      <div class="employeeBody">
+        <p><b>Id:</b> ${manager.id}</p>
+        <p><b>Email:</b> ${manager.email}</p>
+        <p><b>Office Number:</b> ${manager.officeNumber}</p>
+      </div>
+    </div>
+    `;
+  }
 
-// const generateEngineer = engineerInfo => {
-//   return `
-//       ${engineerInfo
-//         .map(({ engineerName, engineerId, engineerEmail, engineerGithub }) => {
-//           return `
-//           <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-//             <h3 class="portfolio-item-title text-light">${engineerName}</h3>
-//             <h5 class="portfolio-languages">
-//               ID: ${engineerId}
-//             </h5>
-//             <p>${engineerEmail}</p>
-//             <p>${engineerGithub}</p>
-//           </div>
-//         `;
-//         })
-//         .join('')}
-//       </div>
-//     </section>
-//   `;
-// };
+  function generateEngineer(engineer) {
+    return `
+      <div class="employeeBorder">
+        <div class="employeeHeader">
+          <label class="eName">${engineer.name}</label>
+          <label class="jobRole">${engineer.getRole()}</label>
+        </div>
+        <div class="employeeBody">
+          <p><b>Id:</b> ${engineer.id}</p>
+          <p><b>Email:</b> ${engineer.email}</p>
+          <p><b>Github:</b> ${engineer.github}</p>
+        </div>
+      </div>
+    `;
+  }
 
-// const generateIntern = internInfo => {
-//   return `
-//       ${internInfo
-//         .map(({ internName, internId, internEmail, internSchool }) => {
-//           return `
-//           <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-//             <h3 class="portfolio-item-title text-light">${internName}</h3>
-//             <h5 class="portfolio-languages">
-//               ID: ${internId}
-//             </h5>
-//             <p>${internEmail}</p>
-//             <p>${internSchool}</p>
-//           </div>
-//         `;
-//         })
-//         .join('')}
-//       </div>
-//     </section>
-//   `;
-// };
+  function generateIntern(intern) {
+    return `
+      <div class="employeeBorder">
+        <div class="employeeHeader">
+          <label class="eName">${intern.name}</label>
+          <label class="jobRole">${intern.getRole()}</label>
+        </div>
+        <div class="employeeBody">
+          <p><b>Id:</b> ${intern.id}</p>
+          <p><b>Email:</b> ${intern.email}</p>
+          <p><b>School:</b> ${intern.school}</p>
+        </div>
+      </div>
+    `;
+  }
 
-module.exports = templateData => {
-  const { manager, engineer, intern, ...header } = templateData;
+  //for loop to push each employee html to the generateTeam function
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].getRole() === "Manager") {
+      html.push(generateManager(data[i]));
+    } else if (data[i].getRole() === "Engineer") {
+      html.push(generateEngineer(data[i]));
+    } else {
+      html.push(generateIntern(data[i]));
+    }
+  }
+  //join html array HTML all together to fit into generateTeam function 
+  return html.join("");
+};
 
+function generateTeamHTML(data) {
   return `
   <!DOCTYPE html>
-  <html lang="en">
-  
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
-  </head>
-  
-  <body>
-    <header>
-      <div class="container flex-row justify-space-between align-center py-3">
-        <h1 class="page-title text-secondary bg-dark py-2 px-3">Team Profile Generator</h1>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>My Team</title>
+      <link rel="stylesheet" href="./style.css" />
+      </head>
+    <body>
+      <div class="pageTitle">
+        <label>My Team</label>
       </div>
-    </header>
-    <section class="my-3" id="portfolio">
-      <h2 class="text-dark bg-primary p-2 display-inline-block align-cetner">Employees</h2>
-      <div class="flex-row justify-space-between">
-    <main class="container my-5">
-      ${generateManager(manager)}
-      ${generateEngineer(engineer)}
-      ${generateIntern(intern)}
-    </main>
-  </body>
-  </html>
+      
+      <div class="mainContainer">
+        ${generateTeamMembers(data)}
+      </div>
+  
+    </body>
+    </html>
   `;
 }
 
